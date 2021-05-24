8835bfe7886e5ab1fc4953c018fb72252ae43cb2
Erin O'Connell <oconerin@oregonstate.edu>
2021-05-24 09:39:07 -0700

    added greeting function

--- main.c
+++ main.c
@@ -4,10 +4,9 @@
 
 int main (int argc, char *argv)
 {
-
-        printf("Greetings!\n\n Welcome to the Solar System!\n\n There are nine planets to explore. \n\n What is your name?");
-        scanf("%s", &value);
-        printf("Hello %s ! Let's  go on an adventure!\n Shall I randomly choose a planet for you to explore?", value);
+	greeting();
+        getName();
+        printf("Shall I randomly choose a planet for you to explore?", value);
 
 	scanf("%s", &YesorNo);
 
@@ -21,6 +20,18 @@ int main (int argc, char *argv)
 
 //end of monolithic approach!
         return 0;
+}
+
+void
+greeting() {
+	printf("Greetings!\n\n Welcome to the Solar System!\n\n There are nine planets to explore. \n\n");
+}
+
+void
+getName() {
+        printf("What is your name?");
+        scanf("%s", &value);
+	printf("Hello %s ! Let's  go on an adventure!\n");
 }
 
 
