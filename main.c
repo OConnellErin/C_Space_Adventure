c4ad543f87db68bab1d03ceba9b547faff375c7b
Erin O'Connell <oconerin@oregonstate.edu>
2021-05-24 08:32:50 -0700

    Added scanning feature

--- main.c
+++ main.c
@@ -5,8 +5,9 @@
 int main (int argc, char *argv)
 {
 
-        printf("Greetings!\n\n Welcome to the Solar System!\n\n There are nine planets to explore. \n\n What is your$
-
+        printf("Greetings!\n\n Welcome to the Solar System!\n\n There are nine planets to explore. \n\n What is your name?");
+        scanf("%s", &value);
+        printf("Hello %s ! Let's  go on an adventure!\n Shall I randomly choose a planet for you to explore?", value);
 
         return 0;
 }
