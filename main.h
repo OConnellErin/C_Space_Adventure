492d5abc1fc8916082d618485c4ea8006f5e5e9e
Erin O'Connell <oconerin@oregonstate.edu>
2021-06-06 23:11:57 -0700

    added planet prototypes

--- main.h
+++ main.h
@@ -6,4 +6,14 @@ void randomPlanet();
 void askForPlanet();
 void getName();
 
+void mercury();
+void venus();
+void earth();
+void mars();
+void jupiter();
+void saturn();
+void uranus();
+void neptune();
+void pluto();
+
 
